c2bb6b164e3f741c507d0d583365107338c9a6e7
Qi Ma <56117354+qimaqi@users.noreply.github.com>
2024-09-25 21:39:47 +0200

    Update __init__.py

--- src/model_zoo/__init__.py
+++ src/model_zoo/__init__.py
@@ -1,4 +1,4 @@
-from src.model_zoo.neuralimage import NeuralImageFunction
+# from src.model_zoo.neuralimage import NeuralImageFunction
 from src.model_zoo.siren import INR, Sine, Siren
-from src.model_zoo.vgg_simplified import Vgg
-from src.model_zoo.semantic_nerf import Semantic_NeRF
+# from src.model_zoo.vgg_simplified import Vgg
+# from src.model_zoo.semantic_nerf import Semantic_NeRF
